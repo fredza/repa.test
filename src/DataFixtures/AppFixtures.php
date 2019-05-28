8fd42e86aa8eef32864768e236fa15cac2d2713a
fredza <fredza801@gmail.com>
2019-05-28 11:41:36 +0200

    section 3 relation avec la bdd correction ds base.html.twig

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ad;
+use App\Entity\Image;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr-FR');
+        for($i = 1; $i <= 30; $i++) {
+            $ad = new Ad();
+            $title      = $faker->sentence();
+            $coverImage = $faker->imageUrl(1000,350);
+            $introduction = $faker->paragraph(2);
+            $content    = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+            $ad->setTitle($title)
+                ->setCoverImage($coverImage)
+                ->setIntroduction($introduction)
+                ->setContent($content)
+                ->setPrice(mt_rand(40, 200))
+                ->setRooms(mt_rand(1, 5));
+
+            $limite = random_int(2,5);
+            for($j = 1; $j <= $limite; $j++) {
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                    ->setCaption($faker->sentence())
+                    ->setAd($ad);
+
+                $manager->persist($image);
+            }
+
+            $manager->persist($ad);
+        }
+
+        $manager->flush();
+    }
+}
